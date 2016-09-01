1835287e4dd96b9ef7d891c5671cfa8b1fafc62e
Annalicia Ostos <annalicia@intagono.com>
2016-08-31 23:27:19 -0500

    Commit Initial

---
+++ Lab04/cellTableTableViewCell.h
@@ -0,0 +1,15 @@
+//
+//  cellTableTableViewCell.h
+//  Lab04
+//
+//  Created by Annalicia Ostos on 31/08/16.
+//  Copyright © 2016 Codelko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface cellTableTableViewCell : UITableViewCell
+@property (strong, nonatomic) IBOutlet UIImageView *imageCell;
+@property (strong, nonatomic) IBOutlet UILabel *nameCell;
+
+@end
---
+++ Lab04/cellTableTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  cellTableTableViewCell.m
+//  Lab04
+//
+//  Created by Annalicia Ostos on 31/08/16.
+//  Copyright © 2016 Codelko. All rights reserved.
+//
+
+#import "cellTableTableViewCell.h"
+
+@implementation cellTableTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
